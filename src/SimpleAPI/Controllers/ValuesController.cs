cc5451d191f89f702005c989e95ad6af309821e9
Srinivas Karthik Putlur <kartb4u@gmail.com>
2019-09-11 15:07:49 +1000

    Broken Unit Test

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Something else";
+            return "Max Power";
         }
 
         // POST api/values
